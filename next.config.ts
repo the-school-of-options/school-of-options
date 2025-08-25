83b87681d5d50a24e1e7527473a46a2e413a6717
nandeeshdb007 <markbotoffical@gmail.com>
2025-08-26 00:57:37 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // 👈 this makes Next.js generate static HTML files in `out/`
+  // you can add other config options here if needed
 };
 
 export default nextConfig;
